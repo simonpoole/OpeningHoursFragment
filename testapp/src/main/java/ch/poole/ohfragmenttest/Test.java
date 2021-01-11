add5b90b77179f820a0bbabe4faf6b38a0edee77
simonpoole <simon@poole.ch>
2021-01-11 11:22:59 +0100

    Use mixed mode in test app

--- testapp/src/main/java/ch/poole/ohfragmenttest/Test.java
+++ testapp/src/main/java/ch/poole/ohfragmenttest/Test.java
@@ -13,43 +13,41 @@ import ch.poole.openinghoursfragment.OnSaveListener;
 import ch.poole.openinghoursfragment.OpeningHoursFragment;
 import ch.poole.openinghoursfragment.ValueWithDescription;
 
-
 public class Test extends AppCompatActivity implements OnSaveListener {
-	final String LONG_TEST = "Mo[2] -2 days 10:00-12:00;24/7;week 4-40 PH+2days dawn-09:00;dawn-25:00/25;2010-2100/4 12:01-13:02, 14:00 , 10:00-(sunset+02:00) , 13:00+, 11:01-45:00/46, dawn-dusk, sunrise+ ; 12-16 closed \"ein test\" ; Mo, We 12:01-13:02 ; Apr-Sep 10:01-13:03, Dec 13:03-21:01";
-	final String SHORT_TEST = "dawn+";
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_test);
-		
-		FragmentManager fm = getSupportFragmentManager();
-		FragmentTransaction ft = fm.beginTransaction();
-	    Fragment prev = fm.findFragmentByTag("fragment_openinghours");
-	    if (prev != null) {
-	        ft.remove(prev);
-	    }
-	    ft.commit();
+    final String LONG_TEST  = "Mo[2] -2 days 10:00-12:00;24/7;week 4-40 PH+2days dawn-09:00;dawn-25:00/25;2010-2100/4 12:01-13:02, 14:00 , 10:00-(sunset+02:00) , 13:00+, 11:01-45:00/46, dawn-dusk, sunrise+ ; 12-16 closed \"ein test\" ; Mo, We 12:01-13:02 ; Apr-Sep 10:01-13:03, Dec 13:03-21:01";
+    final String SHORT_TEST = "dawn+";
 
-	    ArrayList<ValueWithDescription> textValues = new ArrayList<>();
-//	    ValueWithDescription yes = new ValueWithDescription("yes", "Yes");
-//	    textValues.add(yes);
-//	    ValueWithDescription no = new ValueWithDescription("no", "No");
-//	    textValues.add(no);
-//	    
-//	    ValueWithDescription key = new ValueWithDescription("opening_hours", "Opening hours");
-	    ValueWithDescription key = new ValueWithDescription("collection_times", "Collection times");
-        OpeningHoursFragment openingHoursDialog 
-        	= OpeningHoursFragment.newInstance(key,null,R.style.Theme_AppCompat_Light_Dialog_Alert, 5, true, null);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment prev = fm.findFragmentByTag("fragment_openinghours");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.commit();
+
+        ArrayList<ValueWithDescription> textValues = new ArrayList<>();
+        ValueWithDescription yes = new ValueWithDescription("yes", "Yes");
+        textValues.add(yes);
+        ValueWithDescription no = new ValueWithDescription("no", "No");
+        textValues.add(no);
+
+        // ValueWithDescription key = new ValueWithDescription("opening_hours", "Opening hours");
+        ValueWithDescription key = new ValueWithDescription("collection_times", "Collection times");
+        OpeningHoursFragment openingHoursDialog = OpeningHoursFragment.newInstance(key, null, R.style.Theme_AppCompat_Light_Dialog_Alert, 5, true, textValues);
         openingHoursDialog.show(fm, "fragment_openinghours");
-	}
+    }
 
-	@Override
-	public void save(String key, String openingHours) {
-		Log.d("Test", "save got " + openingHours + " for key " + key);
-		TestFragment.showDialog(this, openingHours);
-	}
-	
+    @Override
+    public void save(String key, String openingHours) {
+        Log.d("Test", "save got " + openingHours + " for key " + key);
+        TestFragment.showDialog(this, openingHours);
+    }
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
